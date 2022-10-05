32179462483b526644b69279b499065d2564c73c
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-10-05 12:12:08 +0000

    skończyłem kartę pracy 1

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,10 +1,35 @@
-#zad 1:
+# #zad 1:
 
-a = int(input())
-b = int(input())
-print(a**2 + b**2)
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
 
-#zad 2:
-a = int(input())
-b = int(input())
-print((a+b)**2)
+# #zad 2:
+# a = int(input())
+# b = int(input())
+# print((a+b)**2)
+
+# #zad 3:
+# a = int(input())
+# b = int(input())
+# print((a-b)**3)
+
+# #zad 4:
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a*b*c)
+
+# zad 5:
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+
+#zad 6:
+brutto = float(input())
+print(brutto/1.23)
+
+# #zad 7:
+# a = int(input())
+# b = int(input())
+# print(a%b)
