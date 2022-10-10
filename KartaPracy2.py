c3d051a2f5dcfaab4404e6c6af7b2a2cff6ab9d2
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-10-10 10:20:31 +0000

    zrobiłem zad. 3 i 4

---
+++ KartaPracy2.py
@@ -0,0 +1,21 @@
+# #zad 2
+# n = int(input())
+# if n >= 100 and n < 1000 and n % 17 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# #zad 3
+# n = int(input())
+# if n > 18:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# #zad 4
+# LIMIT = 20
+# waga = int(input())
+# if waga>LIMIT:
+#   print("STÓJ")
+# else:
+#   print("JEDŹ")
