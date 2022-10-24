b1784d532e2b4be1039e3c18948343bb434124d1
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-10-24 10:21:09 +0000

    robię zadanie 5

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,5 +1,27 @@
-#zad 1
-n = int(input())
+# #zad 1
+# n = int(input())
 
-for i in range(n):
- print(i**3 + 3, end=" ")
+# for i in range(n):
+#  print(i**3 + 3, end=" ")
+
+# #zad 2
+# for i in range(105,1000,15):
+#   print(i, end=" ")
+
+# #zad 3
+# n = int(input())
+# for i in range(1, n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
+
+# #zad 4
+# s = 0
+# for i in range(10,100):
+#   s = s + i
+#   print(s)
+
+#zad 5
+n = int(input())
+for  i in range(n-1):
+ k = int(input())
+  
