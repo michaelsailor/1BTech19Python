d196d598aac370a39ea58e45bc32c469c82f77e7
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-10-31 11:46:05 +0000

    skończyłem zad. 6

---
+++ KartaPracy3b.py
@@ -0,0 +1,36 @@
+# #zad 1
+# for i in range(1,31):
+#   print(i, end=" listopada,  ")
+
+# #zad 2
+# for i in range(1,10,2):
+#   print(i**2)
+
+# #zad 3
+# for i in range(1000,10000):
+#   if i%379==0:
+#    print(i, end=" ")
+
+# #zad 4
+# for i in range(100,1000):
+#   if i%5==0:
+#     print(i, end=" ")
+#   elif i%6==0:
+#     print(i, end=" ")
+#   elif i%11==0:
+#     print(i, end=" ")
+
+# #zad 5
+# n = int(input("ile liczb chcesz wpisać? "))
+# suma=0
+# for  i in range(n):
+#   k = int(input())
+#   suma+=k
+# print(suma)
+
+# #zad 6
+# k = int(input())
+# a=0
+# for i in range(2,2*k,2):
+#  a+=i
+# print(a)
