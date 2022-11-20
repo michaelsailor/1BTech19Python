2cdf25547cf2a24935881b60e136a378f700c4f8
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-11-20 14:49:15 +0000

    zrobiłem zad 7

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -11,11 +11,21 @@
 #   else:
 #     print("||", end=" ")
 
-#zad 3
-n = int(input())
-for i in range(1,n+1):
-  print("*", end=" ")
-  if i%2==0:
-    print("--"*i, end=" ")
-  else:
-    print("|"*i, end=" ")
+# #zad 3
+# n = int(input())
+# for i in range(1,n+1):
+#   print("*", end=" ")
+#   if i%2==0:
+#     print("--"*i, end=" ")
+#   else:
+#     print("|"*i, end=" ")
+
+# #zad 7
+# n=int(input())
+# for i in range(n):
+#   for j in range(n):
+#     if i==0 or j==0 or j==n-1 or i==n-1 or (i==n/2 and j==n/2):
+#       print("*", end=" ")
+#     else:
+#       print("-", end =" ")
+#   print("ballz")
