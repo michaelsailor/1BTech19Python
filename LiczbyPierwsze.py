fd4bed50c95910673b2d406f183eccc40ed45e75
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2022-11-28 11:19:41 +0000

    zrobiłem NWD na 2 sposoby

--- LiczbyPierwsze.py
+++ LiczbyPierwsze.py
@@ -1,10 +1,41 @@
-n = int(input("ile liczbpierwszych mam znaleść? "))
+# n = int(input("do ilu mam znaleść liczby pierwsze? "))
 
-for i in range(2,n+1):
-  flaga = True;
-  for j in range(2,i):
-    if i%j==0:
-      flaga = False
-      break
-  if flaga:
-        print(i, end=" ")
+# for i in range(2,n+1):
+#   flaga = True;
+#   for j in range(2,i):
+#     if i%j==0:
+#       flaga = False
+#       break
+#   if flaga:
+#         print(i, end=" ")
+
+# #od n do e liczb 1
+# n = int(input()) 
+# e = int(input()) 
+
+# for i in range(n,e+1):
+#   flaga = True;
+#   for j in range(2,i):
+#     if i%j==0:
+#       flaga = False
+#       break
+#   if flaga:
+#         print(i, end=" ")
+
+# #NWD no.1
+# a=int(input())
+# b=int(input())
+# while a!=b:
+#  if a>b:
+#   a=a-b
+#  elif b>a:
+#   b=b-a
+# if a==b:
+#   print(a)
+
+# #NWD no.2
+# a=int(input())
+# b=int(input())
+# while b>0:
+#   a, b = b, a%b
+# print(a)
