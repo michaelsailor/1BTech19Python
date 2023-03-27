daa16561b6de39b0dbddc58dd296d4e9fe005165
1B29MW <114583585+michaelsailor@users.noreply.github.com>
2023-03-27 10:20:30 +0000

    zadania

--- napisy/nap2.py
+++ napisy/nap2.py
@@ -6,14 +6,37 @@
 # exit("TAK")
 
 
-#anagramy
-a=input()
+# #anagramy
+# a=input()
+# b=input()
+# A=list(a)
+# B=list(b)
+# A.sort()
+# B.sort()
+# if A==B:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# #zadanie 1
+# L=input()
+# print(L[0])
+# print(L[len(L)-1])
+
+# #zadanie 2
+# x=input()
+# print(x[1:len(x)-1])
+
+# #zadanie 3
+# a=input()
+# A=list(a)
+# A.reverse()
+# for i in range(4):
+#   print(A[i])
+
+# zadanie 4
 b=input()
-A=list(a)
-B=list(b)
-A.sort()
-B.sort()
-if A==B:
-  print("TAK")
-else:
-  print("NIE")
+suma=0
+for i in b:
+  suma=suma+ord(i)
+print(suma)
